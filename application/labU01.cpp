6d1400c010de5567c90c3d20fd9b7234edcffe4d
Misael Batista Damasceno <misael19972008@hotmail.com>
2017-09-05 23:34:44 -0300

    implentando

--- application/labU01.cpp
+++ application/labU01.cpp
@@ -4,42 +4,47 @@
 #include <algorithm>
 #include <stdio.h>
 using namespace std;
+void antchave(string & chave){
+    string aux=chave;
+    size_t i=0,j=chave.length();
+    
+    while(i<chave.length()||j>0){
+        chave[i]=aux[j-1]; 
+    i++,j--;
+    }
+}
+void procurarHorizontal (vector <string > &vtr1 ,string & chave,size_t  lin) {
+    for (size_t  i = 0; i < lin; ++i){
+        size_t  posicao = vtr1[i].find(chave);     
+        for (size_t j = posicao; j < chave.length()+posicao; j++){
+            vtr1[i][j] -= 32;
+            
+        }
 
-void procurarHorizontal (string & str ,string & chave) {
-    size_t posicao;
-    posicao = str.find(chave);
-    for (size_t i = posicao; i < chave.size()+posicao; i++)
-        str[i] = toupper(str[i]);
+    }
 
 }
 int main(){ 
-	int lin=20,col=20,contador=0;
-    string str,chave;
-    vector<vector<char> > vtr1;
-    vtr1.reserve(lin); 
+	size_t  lin=20;
+    string chave,lixo;
+    vector<string> vtr1;
+    vtr1.resize(lin); 
     
-    for (int j = 0; j<lin ; j++) {
-        vtr1[j].reserve(col);
+   for (size_t  i = 0; i < lin; ++i){
+    		getline (cin,lixo);
+            vtr1[i]=lixo;
+   }
+
     
-    }
-
-    for (int j = 0; j < lin; ++j){
-    	for (int k = 0; k <col; ++k){
-    		cin >> vtr1[j][k];
-    	}
-    }
-
-    while(contador<19){
+    getline(cin,chave,'\n');
+    while(chave != ""){
+        procurarHorizontal(vtr1,chave,lin);
+        antchave(chave);
+        procurarHorizontal(vtr1,chave,lin);
         getline(cin,chave,'\n');
-        
-        for (int j = 0; j < lin; ++j){
-            for (int k = 0; k <col; ++k){
-                str.push_back(vtr1[j][k]);
-            }
-            procurarHorizontal(str,chave);
-            str.erase();
-        }
-        contador++;
+    }
+    for (size_t i = 0; i <lin ; ++i){
+        cout <<vtr1[i]<< endl; 
     }
     return 0;
 }	
